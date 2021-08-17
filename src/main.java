b5866629870a4865e77f290c3b6c87ea7180a55b
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-17 17:14:31 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -14,14 +14,40 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ScriptBD scpt = new ScriptBD("codigo", Tipos.INT, 100, true, true, true);
-		ScriptBD scpt2 = new ScriptBD("nome", Tipos.VARCHAR, 100, false, false, true);
+		ScriptBD scpt = new ScriptBD("codigo", Tipos.INT, 100, true, true, false, true);
+		ScriptBD scpt2 = new ScriptBD("nome", Tipos.VARCHAR, 100, false, false, true, false);
+		ScriptBD scpt3 = new ScriptBD("endereço", Tipos.VARCHAR, 100, false, false, true, false);
+		ScriptBD scpt4 = new ScriptBD("idade", Tipos.INT, 3, false, false, true, false);
 	
 		
 		Tabela tab = new Tabela("teste");
 		tab.addScript(scpt);
 		tab.addScript(scpt2);
+		tab.addScript(scpt3);
+		tab.addScript(scpt4);
+		
 		System.out.println(tab.criarTabela());
+		
+		Tabela tab2 = new Tabela("teste2");
+		
+		ScriptBD scpt5 = new ScriptBD("codigoFuncionario", Tipos.INT, 100, false,false, false, false);
+		ScriptBD scpt6 = new ScriptBD("nomeFuncionario", Tipos.VARCHAR, 100, false, false, true, false);
+		ScriptBD scpt7 = new ScriptBD("funcao", Tipos.VARCHAR, 100, false, false, true, false);
+		ScriptBD scpt8 = new ScriptBD("setor", Tipos.VARCHAR, 100, false, false, true, false);
+		ScriptBD scpt9 = new ScriptBD("salario", Tipos.FLOAT, 10, false, false, true, false);
+		
+		tab2.addScript(scpt5);
+		tab2.addScript(scpt6);
+		tab2.addScript(scpt7);
+		tab2.addScript(scpt8);
+		tab2.addScript(scpt9);
+		
+		
+		System.out.println(tab2.criarTabelaForeignKey(scpt5.getNome(), tab.getNomeTabela(), scpt.getNome()));
+		
+	
+		
+		
 		
 		ConectarBD conexao = new ConectarBD();
 		conexao.setClassForName("com.mysql.jdbc.Driver");
@@ -37,7 +63,8 @@ public class main {
 		try {
 			conn = conexao.conectar();
 			
-			st = conn.prepareStatement(tab.criarTabela());
+			//st = conn.prepareStatement(tab.criarTabela());
+			st = conn.prepareStatement(tab2.criarTabelaForeignKey(scpt6.getNome(), tab.getNomeTabela(), scpt2.getNome()));
 			
 			int rowsAffected = st.executeUpdate();
 			

ae1a0267b9e44741e99a96fc1fb059b607e0f215
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-17 18:53:54 -0300

    Update Json.java

--- src/JsonCsv/Json.java
+++ src/JsonCsv/Json.java
@@ -1,5 +1,93 @@
 package JsonCsv;
 
-public class Json {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.classes.AbstratTabela;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import Interface.Persistencia;
+
+public class Json implements Persistencia {
+	
+	private String nome;
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public boolean gravar(List<AbstratTabela> lista) {
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.create();
+		FileWriter writer;
+		
+			
+		try {
+			writer = new FileWriter("arquivos/" + getNome() + ".json");
+			writer.write(gson.toJson(lista));
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	
+	public List<AbstratTabela> ler(List<AbstratTabela> tab) {
+		FileReader reader = null;
+
+		try {
+			List<AbstratTabela> tabelas = new ArrayList<>();
+			
+
+			File arquivo = new File("arquivos/" + getNome() + ".json");
+
+			if (arquivo.exists()) {
+				try {
+					reader = new FileReader(arquivo);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				Gson gson = new Gson();
+				
+				tabelas = gson.fromJson(reader, new TypeToken<List<AbstratTabela>>() {
+				}.getType());
+			}
+
+			
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				reader = null;
+			}
+		}
+		return tab;
+		
+	}
+	
+	
 }

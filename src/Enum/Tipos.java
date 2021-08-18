cc614a96fe71d0a7bf2f1dcd551eae57d160026d
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-17 21:19:15 -0300

    Update Tipos.java

--- src/Enum/Tipos.java
+++ src/Enum/Tipos.java
@@ -1,5 +1,9 @@
 package Enum;
-
+/**
+ * 
+ * @author adrianorslsc@hotmail.com
+ *Enum com alguns dos tipos de dados do MySql
+ */
 public enum Tipos {
 	INT, FLOAT, DECIMAL, CHAR, BOOLEAN, VARCHAR, BLOB, MEDIUMTEXT, LONGTEXT, DATE, DATETIME, TIME, character; 
 }

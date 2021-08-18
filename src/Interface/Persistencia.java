d3b610551b393ea3aee4ca72f60b75dbf629c98e
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-08-17 21:19:22 -0300

    Update Persistencia.java

--- src/Interface/Persistencia.java
+++ src/Interface/Persistencia.java
@@ -3,10 +3,24 @@ package Interface;
 import java.util.List;
 
 import com.classes.AbstratTabela;
-
+/**
+ * 
+ * @author adrianorslsc@hotmail.com
+ *
+ */
 public interface Persistencia {
-
+	/**
+	 * Interface usada para gravar os Scripts criados
+	 * @param lista
+	 * @return 
+	 * @throws Exception
+	 */
 	public boolean gravar(List<AbstratTabela> lista)  throws Exception;
-	
+	/**
+	 * Interface usada para ler os Scripts criados
+	 * @param lista
+	 * @return
+	 * @throws Exception
+	 */
 	public List<AbstratTabela> ler(List<AbstratTabela> lista) throws Exception;
 }
